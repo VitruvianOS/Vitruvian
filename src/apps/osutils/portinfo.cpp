5b0c6c87c69d6248e20702d58d4c4c10b334b27e
Barrett17 <b.vitruvio@gmail.com>
2018-06-08 00:07:00 +0200

    Fix portinfo and seminfo build

--- src/apps/osutils/portinfo.cpp
+++ src/apps/osutils/portinfo.cpp
@@ -1,5 +1,7 @@
 #include <OS.h>
 
+#include <OSPrivate.h>
+
 int main(int argc, char **argv)
 {
 	dump_port_info(argc, argv);
--- src/apps/osutils/seminfo.cpp
+++ src/apps/osutils/seminfo.cpp
@@ -1,5 +1,8 @@
 #include <OS.h>
 
+#include <OSPrivate.h>
+
+
 int main(int argc, char **argv)
 {
 	dump_sem_info(argc, argv);

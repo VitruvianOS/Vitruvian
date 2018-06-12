59e1f2df66993c1b244e5185f6d84676e9840229
Barrett17 <b.vitruvio@gmail.com>
2018-06-12 19:22:03 +0200

    atomic: Implement atomic_add using xaddl

--- src/system/kernel_linux/os/atomic.c
+++ src/system/kernel_linux/os/atomic.c
@@ -12,7 +12,7 @@
 static inline int32 atomic_exchange(vint32 *value, int32 old, int32 new)
 {
 	__asm__ __volatile__(
-		"lock; cmpxchg %1, %2"
+		"lock; cmpxchgl %1, %2"
 			: "+a" (old)
 			: "r" (new), "m" (__gcc_noalias__(value))
 			: "memory");
@@ -23,13 +23,12 @@ static inline int32 atomic_exchange(vint32 *value, int32 old, int32 new)
 
 int32 atomic_add(vint32 *value, int32 addvalue)
 {
-	register int32 oldval;
-
-	do {
-		oldval = *value;
-	} while (atomic_exchange(value, oldval, oldval + addvalue) != oldval);
-
-	return oldval;
+      __asm__ volatile("lock; xaddl %0, %1"
+        : "+r" (addvalue), "+m" (*value) // input+output
+        : // No input-only
+        : "memory"
+      );
+      return addvalue;
 }
 
 

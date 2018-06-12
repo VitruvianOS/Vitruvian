76a7bcc34b074c9f301bfd7cca5b9d8f6dbe5514
Barrett17 <b.vitruvio@gmail.com>
2018-06-12 18:26:54 +0200

    atomic: Implement atomic_get using sync_and_fetch_add

--- src/system/kernel_linux/os/atomic.c
+++ src/system/kernel_linux/os/atomic.c
@@ -1,28 +1,8 @@
-//------------------------------------------------------------------------------
-//	Copyright (c) 2004, Bill Hayden
-//
-//	Permission is hereby granted, free of charge, to any person obtaining a
-//	copy of this software and associated documentation files (the "Software"),
-//	to deal in the Software without restriction, including without limitation
-//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//	and/or sell copies of the Software, and to permit persons to whom the
-//	Software is furnished to do so, subject to the following conditions:
-//
-//	The above copyright notice and this permission notice shall be included in
-//	all copies or substantial portions of the Software.
-//
-//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-//	DEALINGS IN THE SOFTWARE.
-//
-//	File Name:		atomic.c
-//	Author:			Bill Hayden (hayden@haydentech.com)
-//	Description:	atomic functions.
-//------------------------------------------------------------------------------
+/* 
+** Copyright (c) 2004, Bill Hayden (hayden@haydentech.com). All rights reserved.
+** Copyright (c) 2018, Dario Casalinuovo
+** Distributed under the terms of the MIT License.
+*/
 
 #include <SupportDefs.h>
 
@@ -77,8 +57,7 @@ int32 atomic_and(vint32 *value, int32 andvalue)
 }
 
 
-//TODO
 int32 atomic_get(vint32 *value)
 {
-	return atomic_or(value, 0);
+	return __sync_fetch_and_add(value, 0);
 }

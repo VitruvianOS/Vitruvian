6e399588e8e22c62ec867cdc245ca6ad65cd0f0b
Barrett17 <b.vitruvio@gmail.com>
2018-06-15 12:24:32 +0200

    Add kernel_memory syscalls

---
+++ src/system/kernel_linux/os/kernel_memory.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2018, Dario Casalinuovo.
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <OS.h>
+
+#include <syscalls.h>
+
+
+status_t
+_kern_get_memory_properties(team_id teamID, const void* address, uint32* _protected,
+	 uint32* _lock)
+{
+	return B_ERROR;
+}

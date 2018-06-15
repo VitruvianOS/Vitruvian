ba48560610f3d21b1be0c66668a04f4b5902cf9d
Barrett17 <b.vitruvio@gmail.com>
2018-06-15 12:31:06 +0200

    Add kernel_time

---
+++ src/system/kernel_linux/os/kernel_time.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2018, Dario Casalinuovo.
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <syscalls.h>
+
+status_t
+_kern_set_timer(int32 timerID, thread_id threadID,
+	bigtime_t startTime, bigtime_t interval, uint32 flags,
+	struct user_timer_info* oldInfo)
+{
+	return B_ERROR;
+}

7a4050a2d978aa47d76a0476a731d4b9241cc5b5
Barrett17 <b.vitruvio@gmail.com>
2019-03-26 17:49:27 +0100

    kernel_linux: Implement system_ioctl

--- src/system/kernel_linux/os/system_ioctl.c
+++ src/system/kernel_linux/os/system_ioctl.c
@@ -1,5 +1,11 @@
+/*
+ ** Copyright 2019, Dario Casalinuovo. All rights reserved
+ ** Distributed under the terms of the LGPL License.
+ */
 
 #include <SupportDefs.h>
+
+#include <sys/ioctl.h>
 
 #include "syscalls.h"
 
@@ -7,13 +13,15 @@
 status_t
 system_ioctl(int fd, uint32 cmd, void *data, size_t length)
 {
-	UNIMPLEMENTED();
-	return B_ERROR;
+	return _kern_ioctl(fd, cmd, data, length);
 }
 
 
 status_t
 _kern_ioctl(int fd, uint32 cmd, void *data, size_t length)
 {
-	return system_ioctl(fd, cmd, data, length);
+	if (ioctl(fd, cmd, data, length) == 0)
+		return B_OK;
+	else
+		return B_ERROR;
 }

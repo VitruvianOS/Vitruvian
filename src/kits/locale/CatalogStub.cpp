97892763ceeeb44a6617d0c5670d9b7101e457e3
Barrett17 <b.vitruvio@gmail.com>
2019-04-09 01:17:54 +0200

    locale: Disable GetCatalog clashing symbols

--- src/kits/locale/CatalogStub.cpp
+++ src/kits/locale/CatalogStub.cpp
@@ -18,11 +18,13 @@ BLocaleRoster::GetCatalog()
 {
 	static BCatalog sCatalog;
 
+#ifdef __HAIKU__
 	#if (__GNUC__ < 3)
 		asm volatile(".hidden GetCatalog__13BLocaleRoster");
 	#else
 		asm volatile(".hidden _ZN13BLocaleRoster10GetCatalogEv");
 	#endif
+#endif
 
 	return _GetCatalog(&sCatalog, &sCatalogInitOnce);
 }

8587b4a4621cea4efca08f67ba4e2cfb86e9b2f0
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-09-06 00:54:29 +0200

    getenv: Add recursive lock

--- src/system/kernel_linux/getenv.c
+++ src/system/kernel_linux/getenv.c
@@ -3,12 +3,24 @@
  * Distributed under the terms of the LGPL License.
  */
 
+#include <pthread.h>
 #include <stdlib.h>
+
+static pthread_mutex_t gMutex;
+static bool gInitialized = false;
 
 
 ssize_t
 __getenv_reentrant(const char* name, char* buffer, size_t bufferSize)
 {
+	if (gInitialized == false) {
+		pthread_mutexattr_t attr;
+		pthread_mutexattr_init(&attr);
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+		pthread_mutex_init(&gMutex, &attr);
+		gInitialized = true;
+	}
+	pthread_mutex_lock(&gMutex);
 	char* ret = getenv(name);
 	if (ret == NULL)
 		return 0;
@@ -16,5 +28,6 @@ __getenv_reentrant(const char* name, char* buffer, size_t bufferSize)
 		strncpy(buffer, ret, bufferSize);
 
 	free(ret);
+	pthread_mutex_unlock(&gMutex);
 	return bufferSize;
 }

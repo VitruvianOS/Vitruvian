dfcf33cad9d51611b9d91f5bc76aa4552769f370
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-11-14 16:21:05 +0100

    kernel_linux: Set global symbols needed by internals

--- src/system/kernel_linux/main.cpp
+++ src/system/kernel_linux/main.cpp
@@ -8,21 +8,33 @@
 #include <kernel/messaging.h>
 
 #include <stdlib.h>
+#include <linux/limits.h>
 
 #include "messaging/MessagingService.h"
 
 
 mode_t __gUmask = 022;
+int32 __gCPUCount;
+int __libc_argc;
+char** __libc_argv;
 
 
 // This function should be called before anything else
 void __attribute__ ((constructor))
-init_kernel_layer()
+init_kernel_layer(int argc, char** argv)
 {
 	printf("init_kernel_layer()\n");
 
+	// Init global stuff
+	__gCPUCount = sysconf(_SC_NPROCESSORS_ONLN);
+	// Note this is not portable on non UNIX systems
+	__libc_argc = argc;
+	__libc_argv = argv;
+
+	// Set screen
 	setenv("TARGET_SCREEN", "root", 1);
 
+	// Init the kernel layer
 	init_area_map();
 	port_init();
 	init_thread();

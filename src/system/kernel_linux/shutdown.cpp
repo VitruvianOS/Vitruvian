b417b5392e490b182d3ee5211c542e51398a1013
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-11-17 20:48:22 +0100

    Implement _kern_shutdown

--- src/system/kernel_linux/shutdown.cpp
+++ src/system/kernel_linux/shutdown.cpp
@@ -5,12 +5,22 @@
 
 #include <SupportDefs.h>
 
+#include <unistd.h>
+#include <linux/reboot.h>
+#include <sys/reboot.h>
+
 #include <syscalls.h>
 
 
 status_t
-_kern_shutdown(bool reboot)
+_kern_shutdown(bool reb)
 {
-	UNIMPLEMENTED();
-	return B_ERROR;
+	sync();
+
+	if (reb == true)
+		reboot(RB_AUTOBOOT);
+	else
+		reboot(LINUX_REBOOT_CMD_POWER_OFF);
+
+	return B_OK;
 }

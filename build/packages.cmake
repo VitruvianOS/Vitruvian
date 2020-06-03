9c74d5083e6a21944c1512f37bb1629bbab28efe
Dario Casalinuovo <b.vitruvio@gmail.com>
2020-06-04 00:58:55 +0200

    buildsystem: Add lists system to include targets to package * Include base system in the package.

--- build/packages.cmake
+++ build/packages.cmake
@@ -1,30 +1,18 @@
  #  Copyright 2019-2020, Dario Casalinuovo. All rights reserved.
  #  Distributed under the terms of the LGPL License.
 
-function( ImageInclude target path )
-	install(TARGETS ${target}
-		COMPONENT ${path}
-		RUNTIME DESTINATION ${path}
-		LIBRARY DESTINATION ${path}
-		DESTINATION ${path}
-	)
+function( ImageInclude path )
+	foreach(arg IN LISTS ARGN)
+		install(TARGETS ${arg}
+			COMPONENT ${path}
+			RUNTIME DESTINATION ${path}
+			LIBRARY DESTINATION ${path}
+			DESTINATION ${path}
+		)
+	endforeach()
 endfunction()
 
-
-ImageInclude(
-	registrar "/system/servers"
-)
-
-ImageInclude(
-	app_server "/system/servers"
-)
-
-ImageInclude(
-	StyledEdit "/system/apps"
-)
-
-
-#include(build/profiles/default)
+include(build/profiles/default)
 
 SET(CPACK_GENERATOR "DEB")
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "KK")

8e99805d0131ba747b49a8d8df3db3f827e94ea3
wesbluemarine <wesbluemarine@gmail.com>
2020-11-22 19:10:47 +0100

    Add Headers and Libraries variabiles for Translators in deps.cmake

--- build/deps.cmake
+++ build/deps.cmake
@@ -1,5 +1,25 @@
 set(BUILD_ICU ON)
 set(ICU_BUILD_VERSION 57.2)
+set(LIBS_PATH_BASE "/usr/lib/x86_64-linux-gnu/")
+set(HEADERS_PATH_BASE "/usr/include")
+
+set(EXR_LIBRARIES "${LIBS_PATH_BASE}/libIlmImf.so;${LIBS_PATH_BASE}/libHalf.so")
+set(EXR_INCLUDES "${HEADERS_PATH_BASE}/OpenEXR/")
+
+set(GIF_LIBRARIES "${LIBS_PATH_BASE}/libgif.so")
+
+set(ICNS_LIBRARIES "${LIBS_PATH_BASE}/libicns.so")
+
+set(JPEG_LIBRARIES "${LIBS_PATH_BASE}/libjpeg.so")
+
+set(PNG_LIBRARIES "${LIBS_PATH_BASE}/libpng.so")
+set(PNG_INCLUDES "${HEADERS_PATH_BASE}/libpng/")
+
+set(TIFF_LIBRARIES "${LIBS_PATH_BASE}/libtiff.so")
+
+set(WEBP_LIBRARIES "${LIBS_PATH_BASE}/libwebp.so")
+
+
 add_subdirectory(external/icu-cmake)
 
 find_package(ZLIB REQUIRED)

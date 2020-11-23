014be94a49f2936ab9a35476216096d993d634f9
wesbluemarine <wesbluemarine@gmail.com>
2020-11-23 12:57:22 +0100

    Add freetype, input, udev  variabiles in deps.cmake

--- build/deps.cmake
+++ build/deps.cmake
@@ -1,14 +1,17 @@
 set(BUILD_ICU ON)
 set(ICU_BUILD_VERSION 57.2)
 set(HEADERS_PATH_BASE "/usr/include")
-set(APPSERVER_LIBRARIES "freetype;input;udev")
 
 set(EXR_LIBRARIES "IlmImf;Half")
 set(EXR_INCLUDES "${HEADERS_PATH_BASE}/OpenEXR/")
 
+set(FREETYPE_LIBRARIES "freetype")
+
 set(GIF_LIBRARIES "gif")
 
 set(ICNS_LIBRARIES "icns")
+
+set(INPUT_LIBRARIES "input")
 
 set(JPEG_LIBRARIES "jpeg")
 
@@ -16,6 +19,8 @@ set(PNG_LIBRARIES "png")
 set(PNG_INCLUDES "${HEADERS_PATH_BASE}/libpng/")
 
 set(TIFF_LIBRARIES "tiff")
+
+set(UDEV_LIBRARIES "udev")
 
 set(WEBP_LIBRARIES "webp")
 

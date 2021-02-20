67599e93d1028e1b2ca41398c722bdbcbbf7a4af
wesbluemarine <wesbluemarine@gmail.com>
2021-02-20 17:19:51 +0100

    Add new dependencies and debian dependencies script generation

--- build/deps.cmake
+++ build/deps.cmake
@@ -15,39 +15,52 @@ macro( DeclareDependency name )
 	set ("${name}_INCLUDES" "${_DECLAREDEPENDENCY_INCLUDES}")
 
 	list (APPEND PACKAGES_LIST "${_DECLAREDEPENDENCY_PACKAGES}")
+	set (DEP_LIST "${DEP_LIST} ${_DECLAREDEPENDENCY_PACKAGES}")
 endmacro()
 
 set(BUILD_ICU ON)
 set(ICU_BUILD_VERSION 57.2)
 set(HEADERS_PATH_BASE "/usr/include")
 
-set(EXR_LIBRARIES "IlmImf;Half")
-set(EXR_INCLUDES "${HEADERS_PATH_BASE}/OpenEXR/")
-
-set(GIF_LIBRARIES "gif")
-
-set(ICNS_LIBRARIES "icns")
-
-set(INPUT_LIBRARIES "input")
-
-set(JPEG_LIBRARIES "jpeg")
-
-set(TIFF_LIBRARIES "tiff")
-
-set(UDEV_LIBRARIES "udev")
-
-set(WEBP_LIBRARIES "webp")
-
-
 add_subdirectory(external/icu-cmake)
 
 find_package(ZLIB REQUIRED)
 
+DeclareDependency(
+	EXR
+	LIBRARIES	"IlmImf;Half"
+	PACKAGES	"libopenexr-dev"
+	INCLUDES	"${HEADERS_PATH_BASE}/OpenEXR/"
+)
 
 DeclareDependency(
 	FREETYPE
 	LIBRARIES	"freetype"
-	PACKAGES	"libfreetype-dev"
+	PACKAGES	"libfreetype6-dev"
+)
+
+DeclareDependency(
+	GIF
+	LIBRARIES	"gif"
+	PACKAGES	"libgif-dev"
+)
+
+DeclareDependency(
+	ICNS
+	LIBRARIES	"icns"
+	PACKAGES	"libicns-dev"
+)
+
+DeclareDependency(
+	INPUT
+	LIBRARIES	"input"
+	PACKAGES	"libinput-dev"
+)
+
+DeclareDependency(
+	JPEG
+	LIBRARIES	"jpeg"
+	PACKAGES	"libjpeg-dev"
 )
 
 DeclareDependency(
@@ -57,4 +70,22 @@ DeclareDependency(
 	INCLUDES	"${HEADERS_PATH_BASE}/libpng/"
 )
 
-#message(STATUS "Dependencies: ${PACKAGES_LIST}")
+DeclareDependency(
+	TIFF
+	LIBRARIES	"tiff"
+	PACKAGES	"libtiff-dev"
+)
+
+DeclareDependency(
+	UDEV
+	LIBRARIES	"udev"
+	PACKAGES	"libudev-dev"
+)
+
+DeclareDependency(
+	WEBP
+	LIBRARIES	"webp"
+	PACKAGES	"libwebp-dev"
+)
+
+file(WRITE build/deb_deps.sh "#!/bin/bash \nsudo apt install ${DEP_LIST} debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools -y")

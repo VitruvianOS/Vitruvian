3f783fa5dbb5c162ed01b2154eface68effad58d
Ivan Gualandri <dreamos82@yahoo.it>
2021-02-21 22:42:28 +0000

    Add return value

--- src/system/kernel_linux/fs/fs_volume.cpp
+++ src/system/kernel_linux/fs/fs_volume.cpp
@@ -12,10 +12,12 @@ _kern_mount(const char* path, const char* device,
 	const char* fs_name, uint32 flags, const char* args,
 	size_t argsLength)
 {
-/*    int mount(const char *source, const char *target,
-                 const char *filesystemtype, unsigned long mountflags,
-                 const void *data);*/
-    mount(path, device, fs_name, flags, args);
+    int mount_status = mount(path, device, fs_name, flags, args);
+    if (mount_status == 0)        
+    {
+        dev_t mounted_path = dev_for_path(path);
+    }
+    return B_ERROR
 }
 
 

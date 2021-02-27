100af1f1022157d52f547f8491c0e79f780a2db5
wesbluemarine <wesbluemarine@gmail.com>
2021-02-28 00:29:39 +0100

    New macro deb runtime dependencies

--- build/deps.cmake
+++ build/deps.cmake
@@ -1,7 +1,7 @@
 macro( DeclareDependency name )
 	set( _OPTIONS_ARGS )
 	set( _ONE_VALUE_ARGS )
-	set( _MULTI_VALUE_ARGS LIBRARIES INCLUDES PACKAGES )
+	set( _MULTI_VALUE_ARGS LIBRARIES INCLUDES PACKAGES RUNTIMES )
 
 	cmake_parse_arguments(
 		_DECLAREDEPENDENCY
@@ -16,6 +16,8 @@ macro( DeclareDependency name )
 
 	list (APPEND PACKAGES_LIST "${_DECLAREDEPENDENCY_PACKAGES}")
 	set (DEP_LIST "${DEP_LIST} ${_DECLAREDEPENDENCY_PACKAGES}")
+	set (RUN_LIST ${RUN_LIST} ${_DECLAREDEPENDENCY_RUNTIMES})
+
 endmacro()
 
 set(BUILD_ICU ON)
@@ -24,12 +26,26 @@ set(HEADERS_PATH_BASE "/usr/include")
 
 add_subdirectory(external/icu-cmake)
 
-find_package(ZLIB REQUIRED)
+DeclareDependency(
+	GNUC
+	RUNTIMES	"libc6 (>= 2.28)"
+)
+
+DeclareDependency(
+	GNUCPP
+	RUNTIMES	"libstdc++6 (>= 5.2)"
+)
+
+DeclareDependency(
+	FLEX
+	RUNTIMES	"libfl2 (>= 2.5.33)"
+)
 
 DeclareDependency(
 	EXR
 	LIBRARIES	"IlmImf;Half"
 	PACKAGES	"libopenexr-dev"
+	RUNTIMES	"libopenexr23 (>= 2.2.1)"
 	INCLUDES	"${HEADERS_PATH_BASE}/OpenEXR/"
 )
 
@@ -37,36 +53,47 @@ DeclareDependency(
 	FREETYPE
 	LIBRARIES	"freetype"
 	PACKAGES	"libfreetype6-dev"
+	RUNTIMES	"libfreetype6 (>= 2.2.1)"
 )
 
 DeclareDependency(
 	GIF
 	LIBRARIES	"gif"
 	PACKAGES	"libgif-dev"
+	RUNTIMES	"libgif7 (>= 5.1.4)"
 )
 
 DeclareDependency(
 	ICNS
 	LIBRARIES	"icns"
 	PACKAGES	"libicns-dev"
+	RUNTIMES	"libicns1 (>= 0.8.1)"
 )
 
 DeclareDependency(
 	INPUT
 	LIBRARIES	"input"
 	PACKAGES	"libinput-dev"
+	RUNTIMES	"libinput10 (>= 0.15.0)"
+)
+
+DeclareDependency(
+	NOTO
+	RUNTIMES	"fonts-noto-core;fonts-noto-extra;fonts-noto-mono"
 )
 
 DeclareDependency(
 	JPEG
 	LIBRARIES	"jpeg"
 	PACKAGES	"libjpeg-dev"
+	RUNTIMES	"libjpeg62-turbo (>= 1.5.2)"
 )
 
 DeclareDependency(
 	PNG
 	LIBRARIES	"png"
 	PACKAGES	"libpng-dev"
+	RUNTIMES	"libpng16-16 (>= 1.6.36)"
 	INCLUDES	"${HEADERS_PATH_BASE}/libpng/"
 )
 
@@ -74,18 +101,33 @@ DeclareDependency(
 	TIFF
 	LIBRARIES	"tiff"
 	PACKAGES	"libtiff-dev"
+	RUNTIMES	"libtiff5 (>= 4.1.0)"
+)
+
+DeclareDependency(
+	TINFO
+	RUNTIMES	"libtinfo6 (>= 6)"
 )
 
 DeclareDependency(
 	UDEV
 	LIBRARIES	"udev"
 	PACKAGES	"libudev-dev"
+	RUNTIMES	"libudev1 (>= 183)"
 )
 
 DeclareDependency(
 	WEBP
 	LIBRARIES	"webp"
 	PACKAGES	"libwebp-dev"
+	RUNTIMES	"libwebp6 (>= 0.6.1)"
+)
+
+DeclareDependency(
+	ZLIB
+	LIBRARIES  "z"
+	PACKAGES  "zlib1g-dev"
+	RUNTIMES  "zlib1g (>= 1:1.1.4)"
 )
 
 file(WRITE build/deb_deps.sh "#!/bin/bash \nsudo apt install ${DEP_LIST} debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools -y")

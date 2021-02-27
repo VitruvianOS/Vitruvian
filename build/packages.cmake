4b37a911e9d32120dcfc70af81cf7ebd3b3e8dd7
wesbluemarine <wesbluemarine@gmail.com>
2021-02-28 00:29:39 +0100

    New macro deb runtime dependencies

--- build/packages.cmake
+++ build/packages.cmake
@@ -27,8 +27,9 @@ endfunction()
 include(build/baseimage.cmake)
 include(build/profiles/default)
 
-
-set(CORE_DEPS "fonts-noto-core, fonts-noto-extra, fonts-noto-mono, libc6 (>= 2.28), libfl2 (>= 2.5.33), libfreetype6 (>= 2.2.1), libgif7 (>= 5.1.4), libicns1 (>= 0.8.1), libinput10 (>= 0.15.0), libncurses6 (>= 6), libopenexr23 (>= 2.2.1), libpng16-16 (>= 1.6.36),libstdc++6 (>= 5.2), libtiff5 (>= 4.1.0), libtinfo6 (>= 6), libudev1 (>= 183), libwebp6 (>= 0.6.1), zlib1g (>= 1:1.1.4)")
+string(REPLACE ";" "," RESULT "${RUN_LIST}")
+message(STATUS "RESULT: ${RESULT}")
+set(CORE_DEPS "${RESULT}")
 
 set(CPACK_DEBIAN_PACKAGE_DEPENDS ${CORE_DEPS})
 SET(CPACK_GENERATOR "DEB")

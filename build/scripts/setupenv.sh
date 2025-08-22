2ee0b269ede6210cb932ae0d407e361ed3201468
Numerio <b.vitruvio@gmail.com>
2025-08-22 12:21:50 +0200

    Update setupenv.sh to be kernel release agnostic

--- build/scripts/setupenv.sh
+++ build/scripts/setupenv.sh
@@ -9,7 +9,7 @@ normal=$(tput sgr0)
 echo ${bold}Install dependencies...
 echo ${normal}
 
-sudo apt install -y debootstrap linux-headers-6.1.0-29-amd64 squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools 
+sudo apt install -y debootstrap linux-headers-amd64 squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
 
 echo  ${bold}Prepare Debian bootstrap and install packages...
 echo ${normal}
@@ -18,10 +18,11 @@ mkdir -p $basedir/LIVE_BOOT
 sudo debootstrap --arch=amd64 --variant=minbase bookworm $basedir/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
 
 sudo chroot $basedir/LIVE_BOOT/chroot /bin/bash -c "echo "vitruvian-live" > /etc/hostname &\
-apt update && apt install -y --no-install-recommends apt-utils dialog linux-image-6.1.0-29-amd64 live-boot systemd-sysv network-manager net-tools wireless-tools curl openssh-client procps vim-tiny libbinutils &&\
+apt update && apt install -y --no-install-recommends apt-utils dialog linux-image-amd64 live-boot systemd-sysv network-manager net-tools wireless-tools curl openssh-client procps vim-tiny libbinutils &&\
 echo "root:vitruvio"|chpasswd; exit"
 
-ls ./LIVE_BOOT/chroot/lib/modules | head -n1 > kernelversion.conf
+ls ./LIVE_BOOT/chroot/lib/modules | head -n1 > imagekernelversion.conf
+uname -r > localkernelversion.conf
 
 echo ${bold}Success!
 echo ${normal}

6d99e69614db0e0963c2106bfacda4d7dc3da54b
Numerio <b.vitruvio@gmail.com>
2025-09-03 13:49:58 +0200

    Update scripts

--- extra/startup.sh
+++ extra/startup.sh
@@ -1,10 +1,5 @@
 #/bin/bash
 
-export LD_PRELOAD=./generated.x86/external/icu-cmake/icu_host/lib/libicudata.so.57
-
-mkdir /dev/shm/vproc
-mkdir /dev/shm/vproc/area
-
 ./src/tests/vos/testharness/clean_shm.sh
 rm /dev/shm/vproc/area/*
 
@@ -14,15 +9,15 @@ sleep 1
 
 script -qc ./generated.x86/src/servers/app/app_server 2&> app_server.out &
 
-#sleep 1
+sleep 1
 
 #script -qc ./generated.x86/src/servers/input/input_server 2&> input_server.out &
 
-sleep 1
+#sleep 5
 
 script -qc ./generated.x86/src/apps/deskbar/Deskbar 2&> deskbar.out &
 
-sleep 1
+sleep 2
 
 script -qc ./generated.x86/src/apps/terminal/Terminal 2&> terminal.out &
 script -qc ./generated.x86/src/apps/pairs/Pairs 2&> pairs.out &

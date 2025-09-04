f9bf35e5c75eb893d832544297b14645007c4ed7
Alberto <14043560+wesbluemarine@users.noreply.github.com>
2025-09-04 09:04:53 +0200

    Remove bootstrap deps
    Packages to install will be listed in the wiki

--- build/scripts/setupenv.sh
+++ build/scripts/setupenv.sh
@@ -6,16 +6,11 @@ basedir=`realpath ./`
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo ${bold}Install dependencies...
-echo ${normal}
-
-sudo apt install -y debootstrap linux-headers-amd64 squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools
-
 echo  ${bold}Prepare Debian bootstrap and install packages...
 echo ${normal}
 
 mkdir -p $basedir/LIVE_BOOT
-sudo debootstrap --arch=amd64 --variant=minbase bookworm $basedir/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
+sudo debootstrap --arch=amd64 --variant=minbase trixie $basedir/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
 
 sudo chroot $basedir/LIVE_BOOT/chroot /bin/bash -c "echo "vitruvian-live" > /etc/hostname &\
 apt update && apt install -y --no-install-recommends apt-utils dialog linux-image-amd64 live-boot systemd-sysv network-manager net-tools wireless-tools curl openssh-client procps vim-tiny libbinutils &&\

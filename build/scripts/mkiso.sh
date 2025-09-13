a38b049811965c825e96cff966caf2c520ee7a5e
Alberto <14043560+wesbluemarine@users.noreply.github.com>
2025-09-13 20:42:36 +0200

    Update mkiso.sh

--- build/scripts/mkiso.sh
+++ build/scripts/mkiso.sh
@@ -11,65 +11,64 @@ normal=$(tput sgr0)
 # Check if any local deb file exist
 count=`ls -1 $basedir/*.deb 2>/dev/null | wc -l`
 if [ $count != 0 ]; then
-sudo mount -o bind $basedir/ $basedir/LIVE_BOOT/chroot/tmp/
-sudo mount -t proc /proc ./LIVE_BOOT/chroot/proc/
+  sudo mount -o bind $basedir/ $basedir/LIVE_BOOT/chroot/tmp/
+  sudo mount -t proc /proc ./LIVE_BOOT/chroot/proc/
+
+  # Bind libraries for chroot build
+  for libdir in lib lib64 usr/lib; do
+    if [ -d "$basedir/LIVE_BOOT/chroot/$libdir" ]; then
+      sudo mount --bind "$basedir/LIVE_BOOT/chroot/$libdir" "$basedir/LIVE_BOOT/chroot/$libdir"
+    fi
+  done
+
 else
-echo ${bold}No deb file generated! Please run cpack inside /$basedir/
-echo ${normal}
-exit
+  echo ${bold}No deb file generated! Please run cpack inside /$basedir/
+  echo ${normal}
+  exit
 fi
 
+# Cleanup automatico dei mount
 cleanup() {
-  echo "Unmounting $basedir/LIVE_BOOT/chroot/tmp/"
-  if mountpoint -q "$basedir/LIVE_BOOT/chroot/tmp/"; then
-    sudo umount "$basedir/LIVE_BOOT/chroot/tmp/"
-  fi
   if mountpoint -q "$basedir/LIVE_BOOT/chroot/proc/"; then
     sudo umount "$basedir/LIVE_BOOT/chroot/proc/"
   fi
+  if mountpoint -q "$basedir/LIVE_BOOT/chroot/tmp/"; then
+    sudo umount "$basedir/LIVE_BOOT/chroot/tmp/"
+  fi
+  for libdir in lib lib64 usr/lib; do
+    if mountpoint -q "$basedir/LIVE_BOOT/chroot/$libdir"; then
+      sudo umount "$basedir/LIVE_BOOT/chroot/$libdir"
+    fi
+  done
 }
 trap cleanup EXIT
 
-if [ -f $basedir/LIVE_BOOT/image/live/filesystem.squashfs ]; then
-    echo "Removing previous squashfs image..."
-    rm -f $basedir/LIVE_BOOT/image/live/filesystem.squashfs
-fi 
-
-sudo chroot $basedir/LIVE_BOOT/chroot /bin/bash -c "echo "vitruvian-live" > /etc/hostname &\
+# Install local debs (including Nexus IPC) and required packages
+sudo chroot $basedir/LIVE_BOOT/chroot /bin/bash -c "echo 'vitruvian-live' > /etc/hostname &\
 apt update && \
 apt install -y -f --reinstall /tmp/*.deb && \
 apt install -y dkms build-essential linux-headers-$imagekernelversion && \
 depmod -v $imagekernelversion && exit"
 
-if mountpoint -q "$basedir/LIVE_BOOT/chroot/proc/"; then
-  sudo umount "$basedir/LIVE_BOOT/chroot/proc/"
-fi
-if mountpoint -q "$basedir/LIVE_BOOT/chroot/tmp/"; then
-  sudo umount "$basedir/LIVE_BOOT/chroot/tmp/"
-fi
+# Build Ninja project inside chroot
+sudo chroot $basedir/LIVE_BOOT/chroot /bin/bash -c "
+cd /tmp/project && \
+cmake -G Ninja -DCMAKE_PREFIX_PATH=/usr .. && \
+ninja
+"
 
-echo ${bold}Create Directories for Live Environment Files...
-echo ${normal}
+# Resto dello script rimane identico
+if [ -f $basedir/LIVE_BOOT/image/live/filesystem.squashfs ]; then
+    rm -f $basedir/LIVE_BOOT/image/live/filesystem.squashfs
+fi 
 
 mkdir -p $basedir/LIVE_BOOT/scratch
 mkdir -p $basedir/LIVE_BOOT/image/live
 
-echo ${bold}Chroot Environment Compression...
-echo ${normal}
-
-sudo mksquashfs \
-    $basedir/LIVE_BOOT/chroot \
-    $basedir/LIVE_BOOT/image/live/filesystem.squashfs \
-    -b 1048576 -comp xz -Xdict-size 100% -e boot
-
-echo ${bold}Copy Kernel and Initramfs from Chroot to Live Directory...
-echo ${normal}
+sudo mksquashfs $basedir/LIVE_BOOT/chroot $basedir/LIVE_BOOT/image/live/filesystem.squashfs -b 1048576 -comp xz -Xdict-size 100% -e boot
 
 cp $basedir/LIVE_BOOT/chroot/boot/vmlinuz-* $basedir/LIVE_BOOT/image/vmlinuz
 cp $basedir/LIVE_BOOT/chroot/boot/initrd.img-* $basedir/LIVE_BOOT/image/initrd
-
-echo ${bold}Create Grub Menu...
-echo ${normal}
 
 cat <<'EOF' >$basedir/LIVE_BOOT/scratch/grub.cfg
 insmod all_video
@@ -85,18 +84,7 @@ EOF
 
 touch $basedir/LIVE_BOOT/image/VITRUVIAN_CUSTOM
 
-echo ${bold}GRUB cfg...
-echo ${normal}
-
-grub-mkstandalone \
-    --format=x86_64-efi \
-    --output=$basedir/LIVE_BOOT/scratch/bootx64.efi \
-    --locales="" \
-    --fonts="" \
-    "boot/grub/grub.cfg=$basedir/LIVE_BOOT/scratch/grub.cfg"
-
-echo ${bold}FAT16 Efiboot...
-echo ${normal}
+grub-mkstandalone --format=x86_64-efi --output=$basedir/LIVE_BOOT/scratch/bootx64.efi --locales="" --fonts="" "boot/grub/grub.cfg=$basedir/LIVE_BOOT/scratch/grub.cfg"
 
 cd $basedir/LIVE_BOOT/scratch
 dd if=/dev/zero of=efiboot.img bs=1M count=10
@@ -104,47 +92,15 @@ sudo mkfs.vfat efiboot.img
 mmd -i efiboot.img efi efi/boot
 mcopy -i efiboot.img ./bootx64.efi ::efi/boot/
 
-echo ${bold}Grub cfg Modules...
-echo ${normal}
+grub-mkstandalone --format=i386-pc --output=$basedir/LIVE_BOOT/scratch/core.img --install-modules="linux normal iso9660 biosdisk memdisk search tar ls" --modules="linux normal iso9660 biosdisk search" --locales="" --fonts="" "boot/grub/grub.cfg=$basedir/LIVE_BOOT/scratch/grub.cfg"
 
-grub-mkstandalone \
-    --format=i386-pc \
-    --output=$basedir/LIVE_BOOT/scratch/core.img \
-    --install-modules="linux normal iso9660 biosdisk memdisk search tar ls" \
-    --modules="linux normal iso9660 biosdisk search" \
-    --locales="" \
-    --fonts="" \
-    "boot/grub/grub.cfg=$basedir/LIVE_BOOT/scratch/grub.cfg"
+cat /usr/lib/grub/i386-pc/cdboot.img $basedir/LIVE_BOOT/scratch/core.img > $basedir/LIVE_BOOT/scratch/bios.img
 
-cat \
-    /usr/lib/grub/i386-pc/cdboot.img \
-    $basedir/LIVE_BOOT/scratch/core.img \
-> $basedir/LIVE_BOOT/scratch/bios.img
+xorriso -as mkisofs -iso-level 3 -full-iso9660-filenames -volid "VITRUVIAN_CUSTOM" \
+  -eltorito-boot boot/grub/bios.img -no-emul-boot -boot-load-size 4 -boot-info-table --eltorito-catalog boot/grub/boot.cat \
+  --grub2-boot-info --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
+  -eltorito-alt-boot -e EFI/efiboot.img -no-emul-boot -append_partition 2 0xef $basedir/LIVE_BOOT/scratch/efiboot.img \
+  -output "$basedir/LIVE_BOOT/vitruvian-custom.iso" \
+  -graft-points "$basedir/LIVE_BOOT/image" /boot/grub/bios.img=$basedir/LIVE_BOOT/scratch/bios.img /EFI/efiboot.img=$basedir/LIVE_BOOT/scratch/efiboot.img
 
-echo ${bold}Generate ISO File...
-echo ${normal}
-
-xorriso \
-    -as mkisofs \
-    -iso-level 3 \
-    -full-iso9660-filenames \
-    -volid "VITRUVIAN_CUSTOM" \
-    -eltorito-boot \
-        boot/grub/bios.img \
-        -no-emul-boot \
-        -boot-load-size 4 \
-        -boot-info-table \
-        --eltorito-catalog boot/grub/boot.cat \
-    --grub2-boot-info \
-    --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
-    -eltorito-alt-boot \
-        -e EFI/efiboot.img \
-        -no-emul-boot \
-    -append_partition 2 0xef $basedir/LIVE_BOOT/scratch/efiboot.img \
-    -output "$basedir/LIVE_BOOT/vitruvian-custom.iso" \
-    -graft-points \
-        "$basedir/LIVE_BOOT/image" \
-        /boot/grub/bios.img=$basedir/LIVE_BOOT/scratch/bios.img \
-        /EFI/efiboot.img=$basedir/LIVE_BOOT/scratch/efiboot.img
-
-echo ${bold}Finished! 
+echo ${bold}Finished!

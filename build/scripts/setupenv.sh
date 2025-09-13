aa84724502694b1bf3da8b5056971cc326dc1904
Debian Live user <wesbluemarine@gmail.com>
2025-09-13 17:56:23 +0000

    Make chroot libraries available for builds

--- build/scripts/setupenv.sh
+++ build/scripts/setupenv.sh
@@ -12,6 +12,13 @@ echo ${normal}
 mkdir -p $basedir/LIVE_BOOT
 sudo debootstrap --arch=amd64 --variant=minbase trixie $basedir/LIVE_BOOT/chroot http://ftp.us.debian.org/debian/
 
+# Bind librerie fondamentali per compilare dentro chroot
+for libdir in lib lib64 usr/lib; do
+  if [ -d "$basedir/LIVE_BOOT/chroot/$libdir" ]; then
+    sudo mount --bind "$basedir/LIVE_BOOT/chroot/$libdir" "$basedir/LIVE_BOOT/chroot/$libdir"
+  fi
+done
+
 sudo chroot $basedir/LIVE_BOOT/chroot /bin/bash -c "echo "vitruvian-live" > /etc/hostname &\
 apt update && apt install -y --no-install-recommends apt-utils dialog linux-image-amd64 live-boot systemd-sysv network-manager net-tools wireless-tools curl openssh-client procps vim-tiny libbinutils &&\
 echo "root:vitruvio"|chpasswd; exit"

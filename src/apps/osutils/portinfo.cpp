d6abbe88d2d339370d55a58d2ceaaae9b8d7dd50
Numerio <b.vitruvio@gmail.com>
2025-10-08 12:57:32 +0200

    apps: cleanup

--- src/apps/osutils/portinfo.cpp
+++
@@ -1,8 +0,0 @@
-#include <OS.h>
-
-#include <OSPrivate.h>
-
-int main(int argc, char **argv)
-{
-	dump_port_info(argc, argv);
-}
--- src/apps/osutils/seminfo.cpp
+++
@@ -1,9 +0,0 @@
-#include <OS.h>
-
-#include <OSPrivate.h>
-
-
-int main(int argc, char **argv)
-{
-	dump_sem_info(argc, argv);
-}

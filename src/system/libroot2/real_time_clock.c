f3ed55f1d6d53c6702ca47b965b9e7ed3c0e0376
Numerio <b.vitruvio@gmail.com>
2025-12-17 04:03:36 +0100

    real_time_clock: Cleanup & fix minor issues

--- src/system/libroot2/real_time_clock.c
+++ src/system/libroot2/real_time_clock.c
@@ -3,35 +3,48 @@
  * Distributed under the terms of the LGPL License.
  */
 
+#include <Debug.h>
+
 #include <time.h>
 #include <stdint.h>
 
 
 void
-set_real_time_clock(uint64 currentTime) {
+set_real_time_clock(uint64_t currentTime)
+{
 	struct timespec ts;
 	ts.tv_sec = (time_t)currentTime;
 	ts.tv_nsec = 0;
-
-	clock_settime(CLOCK_REALTIME, &ts);
+	if (clock_settime(CLOCK_REALTIME, &ts) != 0)
+		debugger("set_real_time_clock: can't set clock");
 }
 
 
 uint64_t
-real_time_clock() {
+real_time_clock()
+{
 	struct timespec ts;
 	clock_gettime(CLOCK_REALTIME, &ts);
-
 	return (uint64_t)ts.tv_sec 
 		+ (uint64_t)ts.tv_nsec / 1000000000;
 }
 
 
 bigtime_t
-real_time_clock_usecs() {
+real_time_clock_usecs()
+{
 	struct timespec ts;
-	clock_gettime(CLOCK_REALTIME, &ts);
+	if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
+		return -1;
+	return (bigtime_t)ts.tv_sec * 1000000LL + (bigtime_t)(ts.tv_nsec / 1000);
+}
 
-	return (bigtime_t)ts.tv_sec * 1000000
-		+ (bigtime_t)ts.tv_nsec / 1000;
+
+uint64_t
+real_time_clock_ns()
+{
+	struct timespec ts;
+	if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
+		return 0;
+	return (uint64_t)ts.tv_sec * 1000000000ULL + (uint64_t)ts.tv_nsec;
 }

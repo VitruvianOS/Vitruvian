5feb8e54fca6eb54294cdb889725246c1c5382ef
Numerio <b.vitruvio@gmail.com>
2025-12-23 18:33:51 +0100

    os: Static assert to check kernel types

--- headers/os/kernel/OS.h
+++ headers/os/kernel/OS.h
@@ -43,10 +43,18 @@ enum {
 typedef int32 area_id;
 typedef int32 port_id;
 typedef int32 sem_id;
-typedef int32 team_id;
-typedef int32 thread_id;
+typedef pid_t team_id;
+typedef pid_t thread_id;
 typedef int32 vref_id;
 
+#if defined(__cplusplus)
+  #define STATIC_ASSERT(expr, msg) static_assert((expr), msg)
+#else
+  #define STATIC_ASSERT(expr, msg) _Static_assert((expr), msg)
+#endif
+
+STATIC_ASSERT(sizeof(pid_t) == sizeof(int32_t),
+	"WARNING: pid_t is not 32-bit on this platform!");
 
 /* Areas */
 

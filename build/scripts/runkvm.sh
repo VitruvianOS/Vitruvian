719b35a923fdc3d6bb51133d27ed359efb359815
Numerio <b.vitruvio@gmail.com>
2026-01-30 16:27:16 +0100

    runkvm: Add host forwarding for debug ssh access

--- build/scripts/runkvm.sh
+++ build/scripts/runkvm.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
 set -e
-
 basedir=`realpath ./`
 
-qemu-system-x86_64 -cdrom ./LIVE_BOOT/vitruvian-custom.iso -boot menu=on -m 8G -cpu host -smp sockets=1,cores=2,threads=2 --enable-kvm
+qemu-system-x86_64 \
+  -cdrom "$basedir/LIVE_BOOT/vitruvian-custom.iso" -boot menu=on \
+  -m 8G -cpu host -smp sockets=1,cores=2,threads=2 --enable-kvm \
+  -netdev user,id=mynet,hostfwd=tcp::2222-:22 \
+  -device virtio-net-pci,netdev=mynet

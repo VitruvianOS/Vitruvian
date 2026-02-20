fdb0a41991112b72d47d450df251360fc05638b6
Numerio <b.vitruvio@gmail.com>
2026-02-20 14:25:24 +0100

    libroot2: Use linux syscall for reboot and shutdown

--- src/system/libroot2/shutdown.cpp
+++ src/system/libroot2/shutdown.cpp
@@ -1,13 +1,14 @@
 /*
- * Copyright 2019, Dario Casalinuovo. All rights reserved.
+ * Copyright 2019-2026, Dario Casalinuovo. All rights reserved.
  * Distributed under the terms of the LGPL License.
  */
 
 #include <SupportDefs.h>
 
-#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
 #include <linux/reboot.h>
-#include <sys/reboot.h>
+#include <sys/syscall.h>
 
 #include <syscalls.h>
 
@@ -15,12 +16,21 @@
 status_t
 _kern_shutdown(bool reb)
 {
+	if (geteuid() != 0) {
+		fprintf(stderr, "_kern_shutdown: Permission denied (not root)\n");
+		return B_NOT_ALLOWED;
+	}
+
 	sync();
 
-	if (reb == true)
-		reboot(RB_AUTOBOOT);
-	else
-		reboot(LINUX_REBOOT_CMD_POWER_OFF);
+	long result = syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
+		reb ? LINUX_REBOOT_CMD_RESTART : LINUX_REBOOT_CMD_POWER_OFF, NULL);
 
-	return B_OK;
+	if (result < 0) {
+		fprintf(stderr, "_kern_shutdown: reboot syscall failed: %s\n",
+			strerror(errno));
+		return B_ERROR;
+	}
+
+	return B_ERROR;
 }

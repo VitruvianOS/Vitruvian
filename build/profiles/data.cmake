d358351168465a04908804dbd5e8196823e35a90
Numerio <b.vitruvio@gmail.com>
2026-04-04 14:36:32 +0200

    Disable befs module.d for now that was causing (non faulty) boot errors

--- build/profiles/data.cmake
+++ build/profiles/data.cmake
@@ -12,7 +12,7 @@ set(SYSTEMD_SERVICES
 
 install(FILES ${SYSTEMD_SERVICES} DESTINATION /etc/systemd/system/)
 
-install(FILES data/etc/modules-load.d/befs.conf DESTINATION /etc/modules-load.d/)
+#install(FILES data/etc/modules-load.d/befs.conf DESTINATION /etc/modules-load.d/)
 
 
 # Boot scripts

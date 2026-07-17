9c83661ecb8d6e6e20a47e5538594573d39d40ab
Numerio <b.vitruvio@gmail.com>
2026-07-17 17:37:12 +0200

    Volume: Fix copyright

--- src/kits/storage/Volume.cpp
+++ src/kits/storage/Volume.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright 2002-2009, Haiku Inc. All Rights Reserved.
- * Copyright 2026, The Vitruvian Project. All Rights Reserved.
+ * Copyright 2026, Dario Casalinuovo <b.vitruvio@gmail.com>. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:

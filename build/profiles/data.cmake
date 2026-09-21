36474be032d35005481d5a8a4b840483324cc8e9
Numerio <b.vitruvio@gmail.com>
2026-09-21 19:08:31 +0200

    build: install vos-partition-lib.sh and DriveSetup polkit helpers
    Completes the install() wiring for the drivesetup privileged-helper
    group (d0c19e4e / 9c18a83a) -- vos-partition-lib.sh and the two
    drivesetup pkexec forwarding helpers/policies were never added to the
    image manifest.

--- build/profiles/data.cmake
+++ build/profiles/data.cmake
@@ -21,6 +21,8 @@ install(PROGRAMS data/libexec/vos-firstboot-commit
 
 install(PROGRAMS data/libexec/vos-install-helper
   DESTINATION /usr/libexec/)
+install(PROGRAMS data/libexec/vos-partition-lib.sh
+  DESTINATION /usr/libexec/)
 
 install(FILES data/etc/installer/excludes.list
   DESTINATION /usr/share/vos/installer/)
@@ -33,6 +35,16 @@ install(FILES data/polkit-1/actions/org.vitruvian.installer.policy
   DESTINATION /usr/share/polkit-1/actions/)
 install(FILES data/polkit-1/rules.d/49-vitruvian-installer.rules
   DESTINATION /usr/share/polkit-1/rules.d/)
+
+install(PROGRAMS data/libexec/vos-drivesetup-helper
+  DESTINATION /usr/libexec/)
+install(FILES data/polkit-1/actions/org.vitruvian.drivesetup.policy
+  DESTINATION /usr/share/polkit-1/actions/)
+
+install(PROGRAMS data/libexec/vos-drivesetup-query
+  DESTINATION /usr/libexec/)
+install(FILES data/polkit-1/actions/org.vitruvian.drivesetup-query.policy
+  DESTINATION /usr/share/polkit-1/actions/)
 
 install(PROGRAMS data/libexec/privilegedguy-helper
   DESTINATION /usr/libexec/)
